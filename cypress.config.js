001f13dda92db80dd383f38f225948d166f8c4ec
Girlea Veaceslav <retrype@gmail.com>
2023-03-22 19:12:21 +0200

    change seting

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    execTimeout: 4000,
+    pageLoadTimeout: 6000,
+    execTimeout: 10000,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
