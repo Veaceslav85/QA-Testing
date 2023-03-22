4964b9974c2d69b63d72f545ee3d0338c3ec090e
Girlea Veaceslav <retrype@gmail.com>
2023-03-22 13:20:10 +0200

    +

--- cypress/e2e/TesteLinkedIn/verificareaLogarii.cy.js
+++ cypress/e2e/TesteLinkedIn/verificareaLogarii.cy.js
@@ -2,7 +2,7 @@ describe('Site LinkedIn', () =>{
     it('Logare',() => {
         cy.visit('https://www.linkedin.com/');
         cy.get('.nav__button-secondary').type('{enter}');
-        cy.get('#username').type('retrype@gmail.com');
+        cy.get('#username').type('retrype@gmail.com'); 
         cy.get('#password').type('');
         cy.get('.btn__primary--large').click();
         cy.get('#phone-input').type('79647421');
