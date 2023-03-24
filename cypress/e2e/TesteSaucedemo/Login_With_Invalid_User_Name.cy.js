60dc1a020084141da1fcf712d4492ce534c48c84
Girlea Veaceslav <retrype@gmail.com>
2023-03-24 19:09:03 +0200

    Add Test (Login With Invalid User)

--- cypress/e2e/TesteSaucedemo/Login_With_Invalid_User_Name.cy.js
+++ cypress/e2e/TesteSaucedemo/Login_With_Invalid_User_Name.cy.js
@@ -7,11 +7,11 @@ describe('Login With Invalid User Name', () => {
     })
     
     it('Verify User can not login to the system using invalid username', () => {
-        cy.get('.login_logo').should('have.value', 'Swag Labs');
+        cy.get('.login_logo').should('have.text', 'Swag Labs');
         cy.get('[data-test=username]').type('invalid_user').should('have.value', 'invalid_user');
         cy.get('[data-test=password]').type('secret_sauce').should('have.value','secret_sauce');
-        cy.get('#login-button').should(hv, 'Login').click();
-        cy.get('[data-test=error]').should(ht, 'Epic sadface: Username and password do not match any user in this service');
+        cy.get('#login-button').should('have.value', 'Login').click();
+        cy.get('[data-test=error]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
 
     })
 })
